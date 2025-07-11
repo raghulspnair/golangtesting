54edc48c0881af8c640bb40e29443beb8ca33650
Raghul SP <raghulsp2002sree@gmail.com>
2025-07-11 15:18:49 +0530

    Add Dockerfile and update Go server

--- testing.go
+++ testing.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Dockerized Go Web App!")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", handler)
+	fmt.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
